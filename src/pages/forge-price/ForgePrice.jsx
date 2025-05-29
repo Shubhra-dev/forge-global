3e1428672f5da40447a5204e191def0183a90081
Shubhra-dev <shuuvro.dev@gmail.com>
2025-05-29 16:52:20 +0600

    browse companies full page added, forge price hero and icon boxes added with responsive and light/dark functionality

--- src/pages/forge-price/ForgePrice.jsx
+++ src/pages/forge-price/ForgePrice.jsx
@@ -1,21 +1,11 @@
 import ForgePriceHero from "./ForgePriceHro";
-import SectionLayout from "../../ui/SectionLayout";
-import IconBox4 from "../../components/IconBox4";
+import IconBoxes from "./IconBoxes";
 
 function ForgePrice() {
   return (
     <>
       <ForgePriceHero />
-      <SectionLayout>
-        <div className="flex flex-col sm:flex-row items-center justify-between gap-8 tab:gap-10">
-          <IconBox4
-            detail={`Forge Price is a derived, indicative price, calculated daily for approximately 200 pre-IPO venture-backed late-stage companies—it synthesizes data from various sources, including secondary market transactions, recent funding rounds, and indications of interest on the Forge platform.`}
-          />
-          <IconBox4
-            detail={`Compared to relying upon standalone sources such as primary funding round prices, last trade price, or public equity proxies, Forge Price provides a more timely and relevant reflection of company value and price discovery—a potential competitive edge for private market participants.`}
-          />
-        </div>
-      </SectionLayout>
+      <IconBoxes />
     </>
   );
 }
