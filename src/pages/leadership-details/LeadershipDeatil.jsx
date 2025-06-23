e0cca44f68659058211440fc0ccb315738cb5ca2
Shubhra-dev <shuuvro.dev@gmail.com>
2025-06-24 01:09:52 +0600

    Leadership detail page all  section added with responsive and light/dark functionality

--- src/pages/leadership-details/LeadershipDeatil.jsx
+++ src/pages/leadership-details/LeadershipDeatil.jsx
@@ -1,124 +1,60 @@
-import Text from "../../components/Text";
 import Title from "../../components/Title";
 import SectionLayout from "../../ui/SectionLayout";
-import Mail from "../../assets/icons/Mail.svg";
-import LinkedIn from "../../assets/icons/LinkedInLight.svg";
-import LinkedInDark from "../../assets/icons/LinkedInDark.svg";
-import X from "../../assets/icons/XLight.svg";
-import XDark from "../../assets/icons/XDark.svg";
-import ExtraSmallText from "../../components/ExtraSmallText";
-import SmallText from "../../components/SmallText";
+import PrimaryButton from "../../components/PrimaryButton";
+import LeadDeatailhero from "./LeadDeatailhero";
 import CEOSmall from "../../assets/CEOSmall.png";
-import SmallHeading from "../../components/SmallHeading";
+import CFO from "../../assets/CFO.jpg";
+import CGO from "../../assets/CGO.jpg";
+import CTO from "../../assets/CTO.jpg";
+import TeamCard from "../leadership/TeamCard";
+
 function LeadershipDeatil() {
   return (
     <>
+      <LeadDeatailhero />
       <SectionLayout
-        padding={`pb-12 pt-[100px] sm:pb-16 sm:pt-[120px] tab:pb-24 tab:pt-[160px]`}
+        bg={`bg-backgroundCream`}
+        bgDark={`dark:bg-backgroundCreamDark`}
       >
-        <div className="w-full flex flex-col-reverse sm:flex-row items-center justify-between">
-          <div className="w-full sm:w-[60%] tab:w-[55%]">
-            <Title align={`text-left`}>
-              Kelly <span className="font-medium">Rodriques</span>
+        <div className="w-full flex flex-col sm:flex-row items-end justify-between gap-4 sm:gap-0">
+          <div className="w-full sm:w-[60%] tab:w-[45%]">
+            <Title
+              fontWeight={`font-medium`}
+              align={`text-center sm:text-left`}
+            >
+              Meet More of Our{" "}
+              <span className="font-semibold text-primary"> Leaders</span>
             </Title>
-            <Text extraClass={`uppercase tracking-widest pt-2.5`}>
-              Chief Executive Officer
-            </Text>
-            <div className="py-8">
-              <Text
-                textColor={`text-textHeading`}
-                textColorDark={`text-textHeadingDark`}
-              >
-                “Like all Finovate team members, I work each day to advance our
-                vision toward a world where everyone can love their life and
-                work. Our commitment to Slalom’s vision-led, purpose-driven,
-                values-based culture has earned us a consistent reputation as
-                one of the best workplaces.”
-              </Text>
-            </div>
-            <div className="flex items-center gap-[15px]">
-              <div className="bg-primary p-2.5 rounded-full">
-                <img src={Mail} alt="mail" />
-              </div>
-              <div>
-                <ExtraSmallText>Email Address</ExtraSmallText>
-                <SmallText>kelly.rodriques@investment.com</SmallText>
-              </div>
-            </div>
           </div>
-          <div className="w-full sm:w-[30%] tab:w-1/4 dark:bg-backgroundDark rounded-md ">
-            <div className="py-5 px-2.5">
-              <img
-                src={CEOSmall}
-                alt="avatar"
-                className="w-48 h-48 rounded-full object-cover object-center m-auto dark:bg-backgroundPrimaryDark"
-              />
-            </div>
-            <div className="flex items-center justify-center gap-4 p-[15px] border-t-2 dark:border-t-backgroundPrimaryDark">
-              <div className=" cursor-pointer p-2.5 dark:hidden rounded-full">
-                <img src={LinkedIn} alt="logo social" />
-              </div>
-              <div className=" cursor-pointer p-2.5 dark:hidden rounded-full">
-                <img src={X} alt="logo social" />
-              </div>
-              <div className="cursor-pointer p-2.5 hidden dark:block dark:bg-backgroundPrimaryDark rounded-full">
-                <img src={LinkedInDark} alt="logo social" />
-              </div>
-              <div className="cursor-pointer p-2.5 hidden dark:block dark:bg-backgroundPrimaryDark rounded-full">
-                <img src={XDark} alt="logo social" />
-              </div>
-            </div>
+          <div className="w-max m-auto sm:m-0">
+            <PrimaryButton>View All</PrimaryButton>
           </div>
         </div>
-        <div className="mt-20 border-b-2">
-          <SmallHeading
-            extraClass={`uppercase tracking-widest pb-5`}
-            fontWeight={`font-medium`}
-          >
-            expertise
-          </SmallHeading>
-        </div>
-        <div className="mt-10 mb-16 flex items-center justify-between flex-wrap space-y-3">
-          <Text
-            textColor={`text-textHeading`}
-            extraClass={`px-5 py-2.5 dark:bg-backgroundPrimaryDark rounded-md`}
-          >
-            Financial Planning
-          </Text>
-          <Text
-            textColor={`text-textHeading`}
-            extraClass={`px-5 py-2.5 dark:bg-backgroundPrimaryDark rounded-md`}
-          >
-            Investment Management
-          </Text>
-          <Text
-            textColor={`text-textHeading`}
-            extraClass={`px-5 py-2.5 dark:bg-backgroundPrimaryDark rounded-md`}
-          >
-            Insurance & Risk Management
-          </Text>
-          <Text
-            textColor={`text-textHeading`}
-            extraClass={`px-5 py-2.5 dark:bg-backgroundPrimaryDark rounded-md`}
-          >
-            Alternative Investment
-          </Text>
-        </div>
-        <div className="tab:w-[60%] m-auto">
-          <SmallHeading fontWeight={`font-normal`} align={`text-justify`}>
-            John is the Worldwide Managing Partner, CEO, and Chairman of the
-            Board of Directors at Finovate, where he oversees global strategy
-            and operations while preserving Bain’s renowned culture. With over
-            20 years at the firm, John has advised global CEOs and is a
-            recognized expert in deal advisory and value creation for portfolio
-            companies. He has a strong background in private equity, corporate
-            mergers and acquisitions, and consumer products. John is also
-            involved in sustainability initiatives and serves on the Board of
-            Directors of Cancer, a non-profit focused on fighting pediatric
-            cancer. He holds master’s degrees in civil engineering and
-            multilingual business communication from the University of Ghent and
-            is based in New York.
-          </SmallHeading>
+        <div className="w-full mt-10 flex flex-wrap items-stretch justify-normal gap-6 tab:gap-5">
+          <TeamCard
+            image={CEOSmall}
+            firstName={`Kelly`}
+            lastName={`Rodriguez`}
+            designation={`Chief Executive Officer`}
+          />
+          <TeamCard
+            firstName={`James`}
+            lastName={`Nevin`}
+            image={CFO}
+            designation={`Chief Financial Officer`}
+          />
+          <TeamCard
+            image={CTO}
+            firstName={`Vidya`}
+            lastName={`Eashwer`}
+            designation={`Chief Technology Officer`}
+          />
+          <TeamCard
+            image={CGO}
+            firstName={`Jennifer`}
+            lastName={`Phillips`}
+            designation={`Chief Growth Officer`}
+          />
         </div>
       </SectionLayout>
     </>
